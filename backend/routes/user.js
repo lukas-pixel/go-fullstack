b8f4165115ac2e5474d7584b99e6c1722f7547c2
marine cherdo <marine.cherdo13@gmail.com>
2023-01-12 14:39:17 +0100

    Créez des tokens d'authentification

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -6,27 +6,4 @@ const userCtrl = require('../controllers/user');
 router.post('/signup', userCtrl.signup);
 router.post('/login', userCtrl.login);
 
-exports.login = (req, res, next) => {
-  User.findOne({email: req.body.email})
-    .then(user => {
-      if (user === null) {
-        res.status(401).json({ message: 'Paire identifiant/mot de passe incorrecte' });
-      } else {
-        bcrypt.compare(req.body.password, user.password)
-          .then(valid => {
-            if (!valid) {
-              res.status(401).json({ message: 'Paire identifiant/mot de passe incorrecte' });
-            } else {
-              res.status(200).json({
-                userId: user._id,
-                token: 'TOKEN'
-              });
-            }
-          })
-          .catch(error => res.status(500).json({ error }));
-      }
-    })
-    .catch(error => res.status(500).json({ error }));
-}
-
 modules.exports = router;
