ed5dab88bbff7f54f985df260b949f121c34fe01
marine cherdo <marine.cherdo13@gmail.com>
2023-01-12 11:07:50 +0100

    configuration des routes d'authentifications

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
 
 mongoose.set("strictQuery", false);
 
@@ -23,5 +24,6 @@ app.use((req, res, next) => {
 });
 
 app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
